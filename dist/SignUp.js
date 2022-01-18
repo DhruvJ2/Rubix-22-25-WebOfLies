8c8013f11191f28dd0a4c2a8e15923166741ee9b
Dhruv Jain <dhruvjain136@gmail.com>
2022-01-18 19:38:27 +0530

    signup

--- dist/SignUp.js
+++ dist/SignUp.js
@@ -1,9 +1,10 @@
 const signupform=document.querySelector('#form');
 signupform.addEventListener('submit',(e)=>{
-e.preventDefault();
-const email=document.getElementById('email').value;
-const pass=document.getElementById('password').value;  
-auth.createUserWithEmailAndPassword(email,pass).then(x=>{
-    console.log(x);
-})  
-})
+    e.preventDefault();
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;  
+
+    firebase.auth().createUserWithEmailAndPassword(email,password).then(x=>{
+        console.log(x);
+    }); 
+});
