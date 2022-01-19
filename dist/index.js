bff111a168f849327f81506acac841002252edc1
Dhruv Jain <dhruvjain136@gmail.com>
2022-01-19 17:23:41 +0530

    Scan Complete

--- dist/index.js
+++ dist/index.js
@@ -45,6 +45,45 @@ custombutton.addEventListener('click', function(){
     realButton.click();
 });
 
-realButton.addEventListener('change', ()=>{
-    console.log("xxxxx");
-});
+// invoice processing
+const submit = document.querySelector('#uploadBtn');
+const done = document.querySelector('.progress');
+
+  realButton.addEventListener("change",function(){
+    var file = this.files[0];
+  
+    if (file) {
+        if ((file.type == 'image/png') ||
+            (file.type == 'image/jpg') ||          
+            (file.type == 'image/jpeg')){
+          
+           var reader = new FileReader();
+  
+            reader.onload = function (evt) {
+                var imgTag = evt.target.result;
+                submit.addEventListener('click', () => {
+                    done.innerText = "Scannig....";
+                    Tesseract.recognize(
+                        imgTag,
+                        'eng',
+                        { logger: m => console.log(m) }
+                    ).then(({ data: { text } }) => {
+                        console.log(text);
+                        done.innerText = "Done!";
+                    });
+                });
+            alert("Image succefully loaded");
+        };
+  
+        reader.onerror = function (evt) {
+          console.error("An error ocurred reading the file",evt);
+        };
+  
+        reader.readAsDataURL(file);
+          
+        }else{
+          alert("Please provide a png or jpg image.");
+          return false;
+        }
+      }
+  },false);
